a2d8c662811b178f9f617ab5713b9050dfd9b5e6
Dextersmart Harry <dextersmart@gmail.com>
2011-11-20 18:05:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoteldesk::Application.config.secret_token = '6f230afadc0d6ee8eada12c57e520ec527ba04cb41cdc546f67edbcd62abb09812aeb4e374f8fa80ccebc7f26caec57422e9e32668166ed8eb17aba2901c9b81'
